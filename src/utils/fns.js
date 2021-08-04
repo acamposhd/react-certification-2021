6c1446c1246d5a218d31c731a30c6bd63968ec50
Alberto Campos <alberto.campos@heyirys.com>
2021-08-03 21:53:48 -0500

    date fns helper function

--- src/utils/fns.js
+++ src/utils/fns.js
@@ -1,5 +1,17 @@
+import { formatDistance } from 'date-fns';
+
 function random(limit) {
   return Math.floor(Math.random() * limit);
 }
 
-export { random };
+const textTruncate = (text = '', limit = 100, replacement = '') => {
+  return text.length <= limit ? text : `${text.substring(0, limit)}${replacement}`;
+};
+
+const getRelativeDate = (oldDate) => {
+  return formatDistance(new Date(oldDate), new Date(), {
+    addSuffix: true,
+  });
+};
+
+export { random, textTruncate, getRelativeDate };
